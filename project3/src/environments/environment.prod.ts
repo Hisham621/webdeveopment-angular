dfc39ee3870219e1a09a024ac5feb5da49ae8d75
unknown <ahamedhishamtp@gmail.com>
2024-12-25 23:44:08 +0530

    add environments and redirects

---
+++ project3/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    url: 'https://localhost:4200', // Replace with your actual URL
+  };
+  
---
+++ project3/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    url: 'https://your-development-url.com', // Replace with your dev URL
+  };
+  
